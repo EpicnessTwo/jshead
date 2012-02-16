411b69c6e4c8b353c1ebd4452de0b20ab9907616
James Booth <boothj5@gmail.com>
2012-02-16 03:30:15 +0000

    Shows details

--- shithead.js
+++ shithead.js
@@ -4,8 +4,7 @@ var playerform;
 var divs;
 var details;
 
-function init()
-{
+function init() {
     console.log("INIT");
     main = document.getElementById("main");
     playerform = document.getElementById('playerform');
@@ -18,32 +17,32 @@ function init()
     details.style.display="none";
 }
 
-function submitCreateGame(form)
-{
+function submitCreateGame(form) {
+    game = new game(form.numplayers.value, form.numcards.value);
+    
     details.style.display="inline";
     main.appendChild(details);
 
     playerform.style.display="none";    
     divs.appendChild(playerform); 
     
-    game = new game(form.numplayers.value, form.numcards.value);
+    document.getElementById("numPlayers").innerHTML = game.numplayers;
+    document.getElementById("numCards").innerHTML = game.numcards;
+
 }
 
-function game(numplayers, numcards)
-{
+function game(numplayers, numcards) {
     this.numplayers = numplayers;
     this.numcards = numcards;
     this.players = new Array();
 }
 
-function player(name, numCards)
-{
+function player(name, numCards) {
     this.name = name;
     this.numCards = numCards;
     this.getDetails = getDetails;
 
-    function getDetails()
-    {
+    function getDetails() {
         return "Name: " + name + ", numCards: " + numCards;
     }
 }
