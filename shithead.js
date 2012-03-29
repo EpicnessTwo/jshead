23bae8a9d20de9d735e999df22262cab3d6ce56f
James Booth <boothj5@gmail.com>
2012-03-29 01:26:03 +0100

    Added player names

--- shithead.js
+++ shithead.js
@@ -1,6 +1,9 @@
+var numplayers = 1
+var maxplayers = 4
+
 var game;
 var main;
-var playerform;
+var gameform;
 var divs;
 var details;
 
@@ -12,33 +15,61 @@ function hide(adiv) {
 function init() {
     console.log("INIT");
     main = document.getElementById("main");
-    playerform = document.getElementById("playerform");
+    gameform = document.getElementById("gameform");
     divs = document.getElementById("divs");
     details = document.getElementById("details");
 
-    main.appendChild(playerform);
-    playerform.style.display="inline"; 
+    main.appendChild(gameform);
+    gameform.style.display="inline"; 
 
     details.style.display="none";
 }
 
+function addplayer() {
+    if (numplayers == maxplayers) {
+        alert("Upto " + maxplayers + " players are allowed");
+    } else {
+        var newdiv = document.createElement('div');
+        newdiv.id = "player" + (numplayers + 1);
+        newdiv.innerHTML = "Player " + (numplayers + 1) + 
+            " name: <input type='text' " + 
+            "id='player" + (numplayers + 1) + "name' " + 
+            "name='player" + (numplayers + 1) + "name'>";
+        document.getElementById("players").appendChild(newdiv);
+        numplayers++;
+    }
+}
+
 function submitCreateGame(form) {
-    game = new game(form.numplayers.value, form.numcards.value);
+    players = new Array(numplayers);
+
+    for (i = 0; i < numplayers; i++) {
+        name = document.getElementById("player" + (i + 1) + "name").value;
+        players[i] = new player(name, form.numcards.value);
+    }
+
+    game = new game(form.numcards.value, players);
     
     details.style.display="inline";
     main.appendChild(details);
 
-    hide(playerform);
+    hide(gameform);
     
-    document.getElementById("numPlayers").innerHTML = game.numplayers;
-    document.getElementById("numCards").innerHTML = game.numcards;
-
+    document.getElementById("details_numcards").innerHTML = game.numcards;
+    playersdiv = document.getElementById("details_players");
+    
+    for (i = 0; i < game.numplayers; i++) {
+        var newdiv = document.createElement('div');
+        newdiv.id = "player" + (i + 1) + "details";
+        newdiv.innerHTML = players[i].name;
+        playersdiv.appendChild(newdiv);
+    }
 }
 
-function game(numplayers, numcards) {
-    this.numplayers = numplayers;
+function game(numcards, players) {
+    this.numplayers = players.length;
     this.numcards = numcards;
-    this.players = new Array();
+    this.players = players;
 }
 
 function player(name, numCards) {
